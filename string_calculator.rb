eefddcf3b4477d74979c95f690143bbdb1cfddfa
user <naveenvardhan777@gmail.com>
2025-07-28 22:49:10 +0530

    supports custom delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,14 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    nums = numbers.split(/,|\n/).map(&:to_i)
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      first_line, numbers = numbers.split("\n", 2)
+      custom_delimiter = first_line[2..]
+      delimiter = Regexp.escape(custom_delimiter)
+    end
+
+    nums = numbers.split(/#{delimiter}/).map(&:to_i)
     nums.sum
   end
 end
