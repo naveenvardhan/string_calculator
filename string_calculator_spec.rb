23ea67d5f2f80ac54d1808490302cc184d71e143
user <naveenvardhan777@gmail.com>
2025-07-28 22:04:55 +0530

    test cases added for comma separated numbers

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe StringCalculator do
   it 'returns 0 for an empty string' do
     expect(calculator.add("")).to eq(0)
   end
+
+  it 'returns sum of two comma-separated values' do
+    expect(calculator.add("1,2")).to eq(3)
+  end
+
+  it 'returns sum of multiple numbers' do
+    expect(calculator.add("1,2,5,7,9,123")).to eq(147)
+  end
 end
