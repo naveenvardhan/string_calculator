dd7f3d45bdfab31108fc676b9a487a9d527ec8d1
user <naveenvardhan777@gmail.com>
2025-07-28 21:46:17 +0530

    Initial commit - adding empty string

---
+++ string_calculator_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'string_calculator'
+
+RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+  
+  it 'returns 0 for an empty string' do
+    expect(calculator.add("")).to eq(0)
+  end
+end
